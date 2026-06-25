0466a02bdda8fecaacc8b09882f4496fa4eb0a21
Ricardo <ricardo270696@gmail.com>
2026-06-25 15:01:13 +0200

    a

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ void pixel_put_image(char *addr, int x, int y, int size_line, int bpp, int color
     *(unsigned int *)dst = color;
 }
 
-void draw_2D_map(t_game *game, int delta_x, int delta_y)
+void draw_2D_map(t_game *game)
 {
 	int color = 0;
 	int y = 0;
@@ -40,37 +40,56 @@ void draw_2D_map(t_game *game, int delta_x, int delta_y)
 		x = 0;
 		while (x < 6)
 		{
-			if (MAP_GRID[y][x] == '0') 
+			if (MAP_GRID[y][x] == '0' || MAP_GRID[y][x] == 'N') 
 				color = 0xFFFFFF;
 			else if (MAP_GRID[y][x] == '1')
 				color = 0x000467;
-			else
-				color = 0x048950;
-			
 			py = 0;
 			while (py < 100)
 			{
 				px = 0;
-				if (color == 0x000467 || color == 0xFFFFFF)
+				while (px < 100)
 				{
-					while (px < 100)
-					{
-						pixel_put_image(game->map_2D->addr, x*100+px, y*100+py, game->map_2D->line_len, game->map_2D->bpp, color);
-						px++;
-					}
-					py++;
+					pixel_put_image(game->map_2D->addr, x*100+px, y*100+py, game->map_2D->line_len, game->map_2D->bpp, color);
+					px++;
 				}
-				if (color == 0x048950)
+				py++;
+			}
+			x++;
+		}
+		y++;
+	}
+}
+
+void draw_pj(t_game *game)
+{
+	int color = 0x868687;
+	int y = 0;
+
+	while (y < 6)
+	{
+		int x = 0;
+		while (x < 6)
+		{
+			if (MAP_GRID[y][x] == 'N')
+			{
+				int py = 0;
+				while (py < 20)
 				{
-					while (px < 100)
+					int px = 0;
+					while (px < 20)
 					{
-						pixel_put_image(game->map_2D->addr, x*100+px+delta_x, y*100+py+delta_y, game->map_2D->line_len, game->map_2D->bpp, color);
+						pixel_put_image(game->map_2D->addr,
+							x * 100 + px + game->player.delta_x,
+							y * 100 + py + game->player.delta_y,
+							game->map_2D->line_len,
+							game->map_2D->bpp, color);
 						px++;
 					}
 					py++;
 				}
 			}
-			x++;
+			x++;          
 		}
 		y++;
 	}
@@ -106,7 +125,8 @@ int handle_keypress(int keycode, t_game *game)
 	if (keycode == W || keycode == A || keycode == S || keycode == D)
 	{
 		move_pj(keycode, &game->player);
-		draw_2D_map(game, game->player.delta_x, game->player.delta_y);
+		draw_2D_map(game);
+		draw_pj(game);
 		mlx_put_image_to_window(game->mlx, game->window,
 			game->map_2D->img_ptr, 0, 0);
 	}
@@ -126,7 +146,8 @@ int main() // poner argc y argv al terminar y descomentar el makefile
 	game.window = mlx_new_window(game.mlx, 600, 600, "cub3D");
 	game.map_2D->img_ptr = mlx_new_image(game.mlx, 600, 600);
 	game.map_2D->addr = mlx_get_data_addr(game.map_2D->img_ptr, &game.map_2D->bpp, &game.map_2D->line_len, &game.map_2D->endian); 
-	draw_2D_map(&game, game.player.delta_x, game.player.delta_y);
+	draw_2D_map(&game);
+	draw_pj(&game);
 	mlx_put_image_to_window(game.mlx, game.window, game.map_2D->img_ptr, 0, 0);
 	mlx_hook(game.window, 17, 0, exit_game, &game);
 	mlx_key_hook(game.window, handle_keypress, &game);
